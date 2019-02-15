d9e83ca745d7571197e393217ce79e9bd34fa7a8
liyupei <liyupei@bytedance.com>
2019-02-15 16:53:51 +0800

    add 1344 1335

---
+++ 1344.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+string str;
+
+int main() {
+    while (cin >> str) {
+        cout << "jin tian shi xingqisi" << endl;
+    }
+    return 0;
+}
